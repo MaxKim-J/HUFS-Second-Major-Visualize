c06aebeec330251846220093737afdbd51df5ed7
김종혁 <hwaseen@gmail.com>
2020-04-29 21:39:05 +0900

    global.css, font.css 파일 작성 기본 폰트 두가지와 body에 기본 배경색 적용

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,26 @@ import ThemeDataChartContainer from './components/container/ThemeDataChartContai
 import AppLayoutContainer from './components/container/AppLayoutContainer'
 import DefaultContent from './components/DefaultContent'
 import Footer from './components/Footer'
+import '../src/font.css'
+import '../src/global.css'
+import styled from 'styled-components'
+
+const AppWrap = styled.div`
+  width: 80%;
+  margin: 0 auto;
+
+  @media screen and (max-width: 750px) {
+    width: 90%;
+  }
+
+  @media screen and (max-width: 640px) {
+    width: 100%;
+  }
+`
 
 const App = () => {
   return (
-    <>
+    <AppWrap>
       <AppLayoutContainer
         header={<HeaderContainer />}
         allDataChart={<AllDataChartContainer />}
@@ -16,7 +32,7 @@ const App = () => {
         defaultContent={<DefaultContent />}
         footer={<Footer />}
       />
-    </>
+    </AppWrap>
   )
 }
 
