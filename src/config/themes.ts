fe5f5f2f0697732bc85121cfcc21e1146f1c0eda
Max Kim <hwaseen@gmail.com>
2020-05-10 01:25:59 +0900

    서울캠 최신데이터 업데이트

--- src/config/themes.ts
+++ src/config/themes.ts
@@ -4,7 +4,7 @@ export type ThemeInfo = {
 }
 
 export const seoulTheme: ThemeInfo[] = [
-  { name: '영어대', icon: '/images/english.png' },
+  { name: '영어대학', icon: '/images/english.png' },
   { name: '프랑스어학부', icon: '/images/france.png' },
   { name: '스페인어과', icon: '/images/spain.png' },
   { name: '마인어과', icon: '/images/indonesia.png' },
@@ -12,9 +12,9 @@ export const seoulTheme: ThemeInfo[] = [
   { name: '중국어대', icon: '/images/china.png' },
   { name: '일본어대', icon: '/images/japan.png' },
   { name: '사과대', icon: '/images/social.png' },
-  { name: '미컴', icon: '/images/broad.png' },
+  { name: '미디어커뮤니케이션학부', icon: '/images/broad.png' },
   { name: '상경대학', icon: '/images/line.png' },
-  { name: '경영학과', icon: '/images/business.png' },
+  { name: '경영학전공', icon: '/images/business.png' },
   { name: 'LD/LT', icon: '/images/ldlt.png' },
   { name: '인기융합전공', icon: '/images/converge.png' },
   { name: '국제학부', icon: '/images/inter.png' },
