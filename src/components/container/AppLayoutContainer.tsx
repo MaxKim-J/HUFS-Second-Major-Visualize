02fccf3b132fe5f47a7f904a131edb522261b5ce
Max Kim <hwaseen@gmail.com>
2020-06-18 22:06:15 +0900

    초기에 차트 로딩시 페이드 애니메이션 추가

--- src/components/container/AppLayoutContainer.tsx
+++ src/components/container/AppLayoutContainer.tsx
@@ -2,16 +2,17 @@ import React, { Suspense } from 'react'
 import { useSelector } from 'react-redux'
 import { RootState } from '../../reducer'
 import { State } from '../../../@types/reducer.d'
-import styled from 'styled-components'
+import styled, { keyframes } from 'styled-components'
 import DefaultContent from '../default/DefaultContent'
 
-const AllDataChartContainer = React.lazy(() =>
-  import('./AllDataChartContainer'),
-)
-const ThemeDataChartContainer = React.lazy(() =>
-  import('./ThemeDataChartContainer'),
-)
-const BaDataChartContainer = React.lazy(() => import('./BaDataChartConatainer'))
+const boxFade = keyframes`
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+`
 
 const Content = styled.div`
   padding-top: 7rem;
@@ -22,34 +23,39 @@ const Content = styled.div`
   @media screen and (max-width: 750px) {
     padding-top: 9rem;
   }
+  animation: ${boxFade} 1s 0s 1 linear alternate;
 `
-
 const Chart = styled.div`
   margin-bottom: 1.5rem;
   padding: 1rem;
   border: 2px solid #f4f4f4;
   border-radius: 20px;
+  animation: ${boxFade} 0.5s 0s 1 linear alternate;
 `
+const AllDataChartContainer = React.lazy(() =>
+  import('./AllDataChartContainer'),
+)
+const ThemeDataChartContainer = React.lazy(() =>
+  import('./ThemeDataChartContainer'),
+)
+const BaDataChartContainer = React.lazy(() => import('./BaDataChartConatainer'))
 
 const AppLayoutContainer = () => {
   const data: State = useSelector((state: RootState) => state.data)
 
+  const charts: JSX.Element[] = [
+    <AllDataChartContainer />,
+    <ThemeDataChartContainer />,
+    <BaDataChartContainer />,
+  ]
   return (
     <Content>
       {data.campusName ? (
-        <>
-          <Suspense fallback={<DefaultContent />}>
-            <Chart>
-              <AllDataChartContainer />
-            </Chart>
-            <Chart>
-              <ThemeDataChartContainer />
-            </Chart>
-            <Chart>
-              <BaDataChartContainer />
-            </Chart>
-          </Suspense>
-        </>
+        <Suspense fallback={<div></div>}>
+          {charts.map(chart => (
+            <Chart>{chart}</Chart>
+          ))}
+        </Suspense>
       ) : (
         <DefaultContent />
       )}
